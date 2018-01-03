fef1f1ccd843fcf0feb3eddc1dd9684973c9dfbd
root <root@localhost.localdomain>
2018-01-03 15:25:21 -0500

    Some benchmarks

--- WorkArea/test.cpp
+++ WorkArea/test.cpp
@@ -1,6 +1,12 @@
-#include <stdio.h>
+#include <iostream>
+//#include <stdio.h>
 
 int main(){
-	for(int i = 0; i < 100000000; ++i) printf("Hellow World!"); 
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	int tmp;
+	for(int i = 0; i < 100000000; ++i)
+		std::cin >> tmp;
+	//	printf("%d\n", tmp);
 	return 0;
 }
